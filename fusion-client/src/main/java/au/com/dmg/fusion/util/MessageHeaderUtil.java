c620e7f374777647fb85bd1ce7e361cd296e91a5
Grace <95324995+i-grace@users.noreply.github.com>
2023-03-12 20:40:48 +1100

    Added generateServiceID without any paramaters required.  This will generate a service id with length = 10.

--- fusion-client/src/main/java/au/com/dmg/fusion/util/MessageHeaderUtil.java
+++ fusion-client/src/main/java/au/com/dmg/fusion/util/MessageHeaderUtil.java
@@ -7,6 +7,10 @@ public class MessageHeaderUtil {
 	private static final String CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	private static SecureRandom random = new SecureRandom();
 
+	public static String generateServiceID() {
+		return generateServiceID(10);
+	}
+
 	public static String generateServiceID(int len) {
 		StringBuilder sb = new StringBuilder(len);
 		for (int i = 0; i < len; i++)
